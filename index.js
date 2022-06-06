4ed5c1016a61b581a00ae1ba6c49d16b74f42a79
hasanex328 <pajak@NB170509BGU0303.intranet.pajak.go.id>
2022-06-06 23:27:17 +0700

    akhirnya bisa... walau telat hahahahah

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
 
+const sorter = (arr, kind) => {
+  const hasil = kind (arr);
+  const hasil2 = ()=>{
+      let arr1 =[]
+      for (i=0;i < hasil.length;i++){
+          arr1.push(`${i+1}. ${hasil[i]}`);
+          }
+      return arr1;
+  }
+  return hasil2();
+}    
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
 
+const sortAscending =(arr) => {
+  return arr.sort();
+}
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  return arr.sort().reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
